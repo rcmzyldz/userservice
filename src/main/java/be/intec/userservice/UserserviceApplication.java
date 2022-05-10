931ab27fa4ee5e18744500bb5b74ef6153c0508b
Rahime Yildiz <rahime1yildiz@gmail.com>
2022-05-10 12:37:06 +0200

    added some methods to the user resource class

--- src/main/java/be/intec/userservice/UserserviceApplication.java
+++ src/main/java/be/intec/userservice/UserserviceApplication.java
@@ -1,7 +1,13 @@
 package be.intec.userservice;
 
+import be.intec.userservice.domain.Role;
+import be.intec.userservice.domain.User;
+import be.intec.userservice.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
@@ -10,4 +16,25 @@ public class UserserviceApplication {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
+	CommandLineRunner run(UserService userService){
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "John Travolta", "john", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Will Smith", "will", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("john", "ROLE_USER");
+			userService.addRoleToUser("will", "ROLE_MANAGER");
+			userService.addRoleToUser("jim", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_USER");
+		};
+
+	}
 }
